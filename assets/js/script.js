4358a6dc9d102d2888959f46f13d3e8c5d713380
Zehra Ecer <zehraecer1996@hotmail.com>
2024-01-19 20:59:51 +0300

    js de değişiklik yapıldı

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
-function cevaplarGizle() {
-    var cevaplar = document.querySelectorAll(".answer");
-    cevaplar.forEach(function (cevap) {
-        cevap.classList.add("cevap");
-    });
-}
+// function cevaplarGizle() {
+//     var cevaplar = document.querySelectorAll(".answer");
+//     cevaplar.forEach(function (cevap) {
+//         cevap.classList.add("cevap");
+//     });
+// }
+//üstteki fonksiyon ilk sayfa açıldığında cevaplar gözükmesin diye oluşturuldu.kaldırdım yine istediğim gbi çalıştı buna gerek yok gibi. .d.d
+
+
 
 function cevaplariGetir(kacinciSoru) {
     // Butona tıklandığında, ilgili cevap div'inin görünürlüğünü değiştir
     var cevaplar = document.getElementById("answer" + kacinciSoru);
     cevaplar.classList.toggle("cevap");
 }
+
+
+
+//toggle sınıfın varlığını tersine çevirir. belirtilen sınıf varsa kaldırır yoksa ekler.
 
  // foreach kullanımı
 // const numbers = [1, 2, 3, 4, 5];
